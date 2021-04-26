850ec1f93dbd1f4ccf4d95959d28a85aeb1e1adb
astrid <astridfernandes1999@gmail.com>
2021-04-26 13:03:01 +0530

    save4

--- isaf2.cpp
+++ isaf2.cpp
@@ -15,4 +15,12 @@ void swap(int *xp, int *yp)
     for (j = 0; j < n-i-1; j++) 
         if (arr[j] > arr[j+1]) 
             swap(&arr[j], &arr[j+1]); 
-}      
+}    
+  /* Function to print an array */
+void printArray(int arr[], int size) 
+{ 
+    int i; 
+    for (i = 0; i < size; i++) 
+        cout << arr[i] << " "; 
+    cout << endl; 
+} 
