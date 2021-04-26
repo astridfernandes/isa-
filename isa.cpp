f9d0739343ea4d574d96de3a50b57a18120572dd
astrid <astridfernandes1999@gmail.com>
2021-04-26 12:22:17 +0530

    save5

--- isa.cpp
+++ isa.cpp
@@ -27,11 +27,4 @@ void printArray(int arr[], int size)
 } 
 // Driver code 
 int main() 
-{ 
-    int arr[] = {64, 34, 25, 12, 22, 11, 90}; 
-    int n = sizeof(arr)/sizeof(arr[0]); 
-    bubbleSort(arr, n); 
-    cout<<"Sorted array: \n"; 
-    printArray(arr, n); 
-    return 0; 
-} 
+
