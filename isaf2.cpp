ea850d0f1bf22ffd27859cfe1a0beb2562f4599c
astrid <astridfernandes1999@gmail.com>
2021-04-26 13:02:20 +0530

    save3

--- isaf2.cpp
+++ isaf2.cpp
@@ -11,3 +11,8 @@ void swap(int *xp, int *yp)
     int i, j; 
 <<<<<<< HEAD
     for (i = 0; i < n-1; i++)
+    	// Last i elements are already in place 
+    for (j = 0; j < n-i-1; j++) 
+        if (arr[j] > arr[j+1]) 
+            swap(&arr[j], &arr[j+1]); 
+}      
