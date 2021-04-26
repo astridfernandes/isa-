5c1e0d880723e9a9db0fac776070ad07f16409a7
astrid <astridfernandes1999@gmail.com>
2021-04-26 13:05:21 +0530

    save5

--- isaf2.cpp
+++ isaf2.cpp
@@ -23,4 +23,13 @@ void printArray(int arr[], int size)
     for (i = 0; i < size; i++) 
         cout << arr[i] << " "; 
     cout << endl; 
+} // Driver code 
+int main() 
+{ 
+    int arr[] = {64, 34, 25, 12, 22, 11, 90}; 
+    int n = sizeof(arr)/sizeof(arr[0]); 
+    bubbleSort(arr, n); 
+    cout<<"Sorted array: \n"; 
+    printArray(arr, n); 
+    return 0; 
 } 
