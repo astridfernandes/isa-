f07f70809641ca01568cca053857af84c0814f09
astrid <astridfernandes1999@gmail.com>
2021-04-26 12:12:32 +0530

    save3

--- isa.cpp
+++ isa.cpp
@@ -11,4 +11,9 @@ void swap(int *xp, int *yp)
 void bubbleSort(int arr[], int n) 
 { 
     int i, j; 
-    for (i = 0; i < n-1; i++)     
+    for (i = 0; i < n-1; i++)
+	// Last i elements are already in place 
+    for (j = 0; j < n-i-1; j++) 
+        if (arr[j] > arr[j+1]) 
+            swap(&arr[j], &arr[j+1]); 
+}      
